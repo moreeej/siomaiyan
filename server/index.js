84c66d9fd07dc4ebf9f8d438b145053351302623
Morej <jeromeaw02@gmail.com>
2025-10-02 22:19:00 +0800

    .

--- server/index.js
+++ server/index.js
@@ -14,23 +14,23 @@ app.use(express.urlencoded({ extended: true }));
 
 const MONGO_URI = process.env.MONGO;
 
-// Test route
+
 app.get("/api/tryserver", (req, res) => {
   res.json({ message: data });
 });
 
-// Connect MongoDB
+
 mongoose.connect(MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
-.then(() => console.log("✅ Connected to MongoDB"))
-.catch(err => console.error("❌ MongoDB connection error:", err));
+.then(() => console.log("Connected to MongoDB"))
+.catch(err => console.error("MongoDB connection error:", err));
 
-// Routes
+
 app.use('/api', userRoutes);
 
-// Start server
+
 app.listen(8080, () => {
   console.log("Server running on port 8080");
 });
